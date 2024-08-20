e86a108e88062788a5fa33bd03459692fd0b2d73
Danilozxz <progdanilo@gmail.com>
2024-08-19 21:40:35 -0300

    Fazendo casting e definindo uma constante

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        // Casting
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Para determinamos uma constante em JAVA adicionamos a palavra reservada "final" e por converção o nome da constante tem que ser em caixa alta
+        final double PI = 3.14;
+        System.out.println(PI);
+    }
+}
